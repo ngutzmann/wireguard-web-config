6f337f6091aeda9feb9a42db6c3d3e6fd6b58ad1
Nathan Gutzmann <ngutzmann@users.noreply.github.com>
2020-04-17 21:02:02 -0600

    Added serverside validation of wireguard keys and ip addresses

--- graph/resolvers/mutations.go
+++ graph/resolvers/mutations.go
@@ -3,8 +3,10 @@ package resolvers
 import (
 	"context"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 	"log"
+	"net"
 	"os/exec"
 	"strings"
 
@@ -12,10 +14,44 @@ import (
 	"github.com/ngutzmann/wireguard-web-config/graph/model"
 )
 
+// WgKeyLength - The Length of a wireguard public key
+const WgKeyLength = 32
+
+func validatePublicKey(publicKey string) error {
+	decoded, err := base64.StdEncoding.DecodeString(publicKey)
+	if err != nil {
+		log.Println("Failed to decode public key:", err)
+		return err
+	}
+	if len(decoded) != WgKeyLength {
+		return fmt.Errorf("decoded public key is incorrect length: was %v, expected %v", len(decoded), WgKeyLength)
+	}
+	return nil
+}
+
+func validateIPAddress(ipAddress string) error {
+	addr := net.ParseIP(ipAddress)
+	if addr == nil {
+		return fmt.Errorf("ip address %v was invalid", ipAddress)
+	}
+	return nil
+}
+
 // CreatePeer - Add a peer to the server
 func CreatePeer(ctx context.Context, db *sql.DB, input model.NewPeer) (*model.Peer, error) {
+
+	err := validatePublicKey(input.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	err = validateIPAddress(input.AllowedIP)
+	if err != nil {
+		return nil, err
+	}
+
 	id := uuid.New()
-	_, err := db.ExecContext(ctx, "INSERT INTO peers (id, user_f_name, user_l_name, hostname, public_key, allowed_ip) VALUES ($1, $2, $3, $4, $5, $6)", id, input.UserFName, input.UserLName, input.Hostname, input.PublicKey, input.AllowedIP)
+	_, err = db.ExecContext(ctx, "INSERT INTO peers (id, user_f_name, user_l_name, hostname, public_key, allowed_ip) VALUES ($1, $2, $3, $4, $5, $6)", id, input.UserFName, input.UserLName, input.Hostname, input.PublicKey, input.AllowedIP)
 
 	if err != nil {
 		log.Println("Failed to create peer entry in DB:", err)
